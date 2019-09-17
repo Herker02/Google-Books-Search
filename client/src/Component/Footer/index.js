8e732179dc29321dca2892c2bbd6266522ef3464
HerKer02 <herker02@gmail.com>
2019-09-17 16:47:01 -0500

    frontend

---
+++ client/src/Component/Footer/index.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>Ker Her</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
